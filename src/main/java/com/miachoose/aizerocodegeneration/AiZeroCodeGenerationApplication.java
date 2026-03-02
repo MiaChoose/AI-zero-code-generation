96ff7d05ce1e5004f7f2cfb6f97a2836a1167978
He <hzyloong@163.com>
2026-03-02 17:52:33 +0800

    test

---
+++ src/main/java/com/miachoose/aizerocodegeneration/AiZeroCodeGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.miachoose.aizerocodegeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiZeroCodeGenerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiZeroCodeGenerationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miachoose/aizerocodegeneration/AiZeroCodeGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miachoose.aizerocodegeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiZeroCodeGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
