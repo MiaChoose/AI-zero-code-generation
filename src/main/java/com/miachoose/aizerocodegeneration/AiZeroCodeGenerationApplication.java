bab56e1cdc60f484839fd764aabb1c1a67224941
He <hzyloong@163.com>
2026-03-03 10:00:03 +0800

    上线用户模块

--- src/main/java/com/miachoose/aizerocodegeneration/AiZeroCodeGenerationApplication.java
+++ src/main/java/com/miachoose/aizerocodegeneration/AiZeroCodeGenerationApplication.java
@@ -1,9 +1,11 @@
 package com.miachoose.aizerocodegeneration;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.miachoose.aizerocodegeneration.mapper")
 public class AiZeroCodeGenerationApplication {
 
     public static void main(String[] args) {
