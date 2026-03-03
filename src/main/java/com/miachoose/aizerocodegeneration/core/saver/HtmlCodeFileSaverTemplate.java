76f6b35c6224934dbcc56fb0050449fdf3dbecca
He <hzyloong@163.com>
2026-03-03 14:20:59 +0800

    代码生成

---
+++ src/main/java/com/miachoose/aizerocodegeneration/core/saver/HtmlCodeFileSaverTemplate.java
@@ -0,0 +1,35 @@
+package com.miachoose.aizerocodegeneration.core.saver;
+
+import cn.hutool.core.util.StrUtil;
+import com.miachoose.aizerocodegeneration.ai.model.HtmlCodeResult;
+import com.miachoose.aizerocodegeneration.exception.BusinessException;
+import com.miachoose.aizerocodegeneration.exception.ErrorCode;
+import com.miachoose.aizerocodegeneration.model.enums.CodeGenTypeEnum;
+
+/**
+ * HTML代码文件保存器
+ *
+ * @author yupi
+ */
+public class HtmlCodeFileSaverTemplate extends CodeFileSaverTemplate<HtmlCodeResult> {
+
+    @Override
+    protected CodeGenTypeEnum getCodeType() {
+        return CodeGenTypeEnum.HTML;
+    }
+
+    @Override
+    protected void saveFiles(HtmlCodeResult result, String baseDirPath) {
+        // 保存 HTML 文件
+        writeToFile(baseDirPath, "index.html", result.getHtmlCode());
+    }
+
+    @Override
+    protected void validateInput(HtmlCodeResult result) {
+        super.validateInput(result);
+        // HTML 代码不能为空
+        if (StrUtil.isBlank(result.getHtmlCode())) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "HTML代码内容不能为空");
+        }
+    }
+}
