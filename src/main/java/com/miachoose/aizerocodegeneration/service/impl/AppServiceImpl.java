d460b05e62ed5e651b75014d476133c50fa1931d
He <hzyloong@163.com>
2026-03-06 17:37:29 +0800

    项目生成封面图

--- src/main/java/com/miachoose/aizerocodegeneration/service/impl/AppServiceImpl.java
+++ src/main/java/com/miachoose/aizerocodegeneration/service/impl/AppServiceImpl.java
@@ -23,6 +23,7 @@ import com.miachoose.aizerocodegeneration.model.vo.AppVO;
 import com.miachoose.aizerocodegeneration.model.vo.UserVO;
 import com.miachoose.aizerocodegeneration.service.AppService;
 import com.miachoose.aizerocodegeneration.service.ChatHistoryService;
+import com.miachoose.aizerocodegeneration.service.ScreenshotService;
 import com.miachoose.aizerocodegeneration.service.UserService;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
@@ -62,6 +63,9 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
     private StreamHandlerExecutor streamHandlerExecutor;
     @Resource
     private VueProjectBuilder vueProjectBuilder;
+    @Resource
+    private ScreenshotService screenshotService;
+
 
     @Override
     public Flux<String> chatToGenCode(Long appId, String message, User loginUser) {
@@ -162,14 +166,33 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         boolean updateResult = this.updateById(updateApp);
         ThrowUtils.throwIf(!updateResult, ErrorCode.OPERATION_ERROR, "更新应用部署信息失败");
         // 10. 返回可访问的 URL
-        return String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        String appDeployUrl = String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        // 11. 异步生成截图并且更新应用封面
+        generateAppScreenshotAsync(appId, appDeployUrl);
+        return appDeployUrl;
     }
 
-
+    /**
+     * 异步生成应用截图并更新封面
+     *
+     * @param appId  应用ID
+     * @param appUrl 应用访问URL
+     */
     @Override
     public void generateAppScreenshotAsync(Long appId, String appUrl) {
-
+        // 使用虚拟线程异步执行
+        Thread.startVirtualThread(() -> {
+            // 调用截图服务生成截图并上传
+            String screenshotUrl = screenshotService.generateAndUploadScreenshot(appUrl);
+            // 更新应用封面字段
+            App updateApp = new App();
+            updateApp.setId(appId);
+            updateApp.setCover(screenshotUrl);
+            boolean updated = this.updateById(updateApp);
+            ThrowUtils.throwIf(!updated, ErrorCode.OPERATION_ERROR, "更新应用封面字段失败");
+        });
     }
+
 
     @Override
     public AppVO getAppVO(App app) {
